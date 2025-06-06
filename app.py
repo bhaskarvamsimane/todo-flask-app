2439bdc9e6aa4390cd853b0b5a1b75c5d82072b2
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-06 22:27:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -27,3 +28,7 @@ def delete(task_id):
     if 0 <= task_id < len(tasks):
         tasks.pop(task_id)
     return redirect('/')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # get port from Render
+    app.run(host="0.0.0.0", port=port)        # listen on all interfaces
